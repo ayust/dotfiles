610108a7d1f7fcef2b2c5c3183637276758a22e8
Amber Yust <amber.yust@gmail.com>
2012-08-06 20:35:11 -0700

    Putty and X settings

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,23 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/aiiane/.xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "/usr/bin/xlock")
+        , ((mod4Mask .|. shiftMask, xK_Return), spawn "/usr/bin/pterm")
+        , ((controlMask, xK_Print), spawn "sleep 0.2; /usr/bin/scrot -s")
+        , ((0, xK_Print), spawn "/usr/bin/scrot")
+        ]
