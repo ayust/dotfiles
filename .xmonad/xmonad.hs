b675a3178c9a08937ace4b6ce70af466dd959abc
Amber Yust <aiiane@google.com>
2012-08-08 13:57:35 -0700

    Add fetchotp keybinding, autofloats, remove bg color

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,10 +5,16 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
+myManageHook = composeAll
+    [ className =? "Gimp"      --> doFloat
+    , className =? "Vncviewer" --> doFloat
+    ]
+
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar /home/aiiane/.xmobarrc"
     xmonad $ defaultConfig
-        { manageHook = manageDocks <+> manageHook defaultConfig
+        { manageHook = manageDocks <+> myManageHook
+                        <+> manageHook defaultConfig
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
@@ -17,6 +23,7 @@ main = do
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "/usr/bin/gnome-screensaver-command -l")
+        , ((mod4Mask .|. shiftMask, xK_i), spawn "/usr/bin/fetchotp -x")
         , ((mod4Mask .|. shiftMask, xK_Return), spawn "/usr/bin/pterm")
         , ((controlMask, xK_Print), spawn "sleep 0.2; /usr/bin/scrot -s")
         , ((0, xK_Print), spawn "/usr/bin/scrot")
