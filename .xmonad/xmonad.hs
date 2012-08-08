8cfaac7c603daee50717ad4aab6bbce6e7b36b55
Amber Yust <amber.yust@gmail.com>
2012-08-08 11:24:25 -0700

    xmonad tweaks

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -16,7 +16,7 @@ main = do
                         }
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
         } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "/usr/bin/xlock")
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "/usr/bin/gnome-screensaver-command -l")
         , ((mod4Mask .|. shiftMask, xK_Return), spawn "/usr/bin/pterm")
         , ((controlMask, xK_Print), spawn "sleep 0.2; /usr/bin/scrot -s")
         , ((0, xK_Print), spawn "/usr/bin/scrot")
