a3b730e7f72e21e9ecb6277ea7e1b3039ca2a5e7
Amber Yust <aiiane@google.com>
2012-08-14 16:30:53 -0700

    Add volume keys for laptop

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,6 +4,7 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
+import Graphics.X11.ExtraTypes.XF86
 
 myManageHook = composeAll
     [ className =? "Gimp"      --> doFloat
@@ -26,6 +27,9 @@ main = do
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "/usr/bin/gnome-screensaver-command -l")
         , ((mod4Mask .|. shiftMask, xK_i), spawn "/usr/bin/fetchotp -x")
+        , ((0, xF86XK_AudioLowerVolume), spawn "/usr/bin/amixer set Master 2dB-")
+        , ((0, xF86XK_AudioRaiseVolume), spawn "/usr/bin/amixer set Master 2dB+")
+        , ((0, xF86XK_AudioMute), spawn "/usr/bin/amixer set Master toggle")
         , ((controlMask, xK_Print), spawn "sleep 0.2; /usr/bin/scrot -s")
         , ((0, xK_Print), spawn "/usr/bin/scrot")
         ]
