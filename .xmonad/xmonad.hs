2c1868109d05c546ffc5248b5e81c5da8a5f5286
Amber Yust <aiiane@google.com>
2014-10-31 17:06:14 -0700

    Spawn xcompmgr to work around nvidia bug

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -31,31 +31,37 @@ myManageHook = composeAll
 myUrgencyHook = SpawnUrgencyHook "~/bin/urgency_hook "
 myUrgencyConfig = urgencyConfig { suppressWhen = Visible }
 
+myConfig = withUrgencyHookC myUrgencyHook myUrgencyConfig $ defaultConfig
+    { manageHook = manageDocks <+> myManageHook
+                    <+> manageHook defaultConfig
+    , layoutHook = avoidStruts  $  smartBorders  $  layoutHook defaultConfig
+    , handleEventHook = fullscreenEventHook
+    , startupHook = do
+        startupHook myConfig
+        spawn "/usr/bin/xcompmgr"
+    , modMask = mod4Mask     -- Rebind Mod to the Windows key
+    , terminal = "terminator"
+    } `additionalKeys`
+    [ ((mod4Mask .|. shiftMask, xK_z), spawn "/usr/bin/gnome-screensaver-command -l")
+    , ((mod4Mask .|. shiftMask, xK_x), spawn "/usr/bin/gksudo /usr/sbin/pm-suspend-hybrid")
+    , ((mod4Mask .|. shiftMask, xK_i), spawn "/usr/bin/xcalib -a -i")
+    , ((mod4Mask .|. shiftMask, xK_g), focusUrgent)
+    , ((mod4Mask .|. shiftMask, xK_q), quitWithWarning)
+    , ((mod4Mask, xK_b), sendMessage ToggleStruts)
+    , ((mod4Mask, xK_quoteleft), spawn "~/bin/touchpad-toggle")
+    , ((0, xF86XK_AudioLowerVolume), spawn "/usr/bin/amixer set Master 2dB-")
+    , ((0, xF86XK_AudioRaiseVolume), spawn "/usr/bin/amixer set Master 2dB+")
+    , ((0, xF86XK_AudioMute), spawn "/usr/bin/amixer set Master toggle")
+    , ((controlMask, xK_Print), spawn "sleep 0.2; /usr/bin/scrot -s")
+    , ((0, xK_Print), spawn "/usr/bin/scrot")
+    ]
+
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar $HOME/.xmonad/.xmobarrc"
-    xmonad $ withUrgencyHookC myUrgencyHook myUrgencyConfig $ defaultConfig
-        { manageHook = manageDocks <+> myManageHook
-                        <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  smartBorders  $  layoutHook defaultConfig
-        , handleEventHook = fullscreenEventHook
-        , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
-                        , ppUrgent = xmobarColor "red" "" . xmobarStrip
-                        }
-        , modMask = mod4Mask     -- Rebind Mod to the Windows key
-        , terminal = "terminator"
-        } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "/usr/bin/gnome-screensaver-command -l")
-        , ((mod4Mask .|. shiftMask, xK_x), spawn "/usr/bin/gksudo /usr/sbin/pm-suspend-hybrid")
-        , ((mod4Mask .|. shiftMask, xK_i), spawn "/usr/bin/xcalib -a -i")
-        , ((mod4Mask .|. shiftMask, xK_g), focusUrgent)
-        , ((mod4Mask .|. shiftMask, xK_q), quitWithWarning)
-        , ((mod4Mask, xK_b), sendMessage ToggleStruts)
-        , ((mod4Mask, xK_quoteleft), spawn "~/bin/touchpad-toggle")
-        , ((0, xF86XK_AudioLowerVolume), spawn "/usr/bin/amixer set Master 2dB-")
-        , ((0, xF86XK_AudioRaiseVolume), spawn "/usr/bin/amixer set Master 2dB+")
-        , ((0, xF86XK_AudioMute), spawn "/usr/bin/amixer set Master toggle")
-        , ((controlMask, xK_Print), spawn "sleep 0.2; /usr/bin/scrot -s")
-        , ((0, xK_Print), spawn "/usr/bin/scrot")
-        ]
+    xmonad $ myConfig
+        { logHook = dynamicLogWithPP xmobarPP
+                    { ppOutput = hPutStrLn xmproc
+                    , ppTitle = xmobarColor "green" "" . shorten 50
+                    , ppUrgent = xmobarColor "red" "" . xmobarStrip
+                    }
+        }
